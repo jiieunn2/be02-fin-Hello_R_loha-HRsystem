83c3aa21f1932ffc5f9ba19eef4db81042d30f51
LCH-97 <leechanghoon12@gmail.com>
2024-03-07 16:41:42 +0900

    [Feat] 휴가 메소드 구현 (#12, #23, #24, #25)
    요구사항 명세서에 맞춰 Req/Res Entity를 만들고 Service/Controller에 Create, Read, List, Delete, Update, UpdateStatus 메소드를 구현하였음.
    그러나 개발시간이 부족하여 일단 임시로 휴가결재라인은 제외하고 구연하였고, 휴가유형클래스 대신 휴가유형이라는 String타입의 변수를 만들어 구현하였음.
    아직 초기라 예를들어 status(결재 상태. 기본상태/결재완료상태/반려상태)를 기존에 정해둔 것 외의 값을 입력하거나 하는 등 여러 메소드에서 허점이 많은데 예외처리가 거의 되어있지 않은 상태.

--- backend/src/main/java/com/HelloRolha/HR/feature/employee/model/entity/Employee.java
+++ backend/src/main/java/com/HelloRolha/HR/feature/employee/model/entity/Employee.java
@@ -1,17 +1,18 @@
 package com.HelloRolha.HR.feature.employee.model.entity;
 import com.HelloRolha.HR.common.entity.BaseEntity;
 import com.HelloRolha.HR.feature.department.model.entity.Department;
+import com.HelloRolha.HR.feature.goout.model.Goout;
+import com.HelloRolha.HR.feature.goout.model.GooutFile;
 import com.HelloRolha.HR.feature.position.model.entity.Position;
 
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -22,9 +23,17 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class Employee extends BaseEntity {
 
+    //
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer remainingVacationDays;
+    private String position;
+    //
+
     private String username;
     private String password;
-    private String authory;
+    private String authority;
     private Integer employeeNum;
     private LocalDate employmentDate;
     private Boolean status;
@@ -39,5 +48,10 @@ public class Employee extends BaseEntity {
     //외래키 TODO 관계 추가해야함
 //    private Department department;
 //    private Position position;
+
+    //
+    @OneToMany(mappedBy = "employee")
+    private List<Goout> goouts = new ArrayList<>();
+    //
 
 }
