cf045d0a04a9af5bb8aa1d7139dd402d0d5268ba
LCH-97 <leechanghoon12@gmail.com>
2024-03-08 16:23:15 +0900

    [Feat] 휴가유형 기능 구현 (#73, #74, #75, #76, #77)
    인사담당자가 노동법과 회사규정에 따른 휴가유형, 갯수, 설명 등을 CRUD할 수 있도록 기능을 구현하였음

---
+++ backend/src/main/java/com/HelloRolha/HR/feature/goout/service/GooutTypeService.java
@@ -0,0 +1,74 @@
+package com.HelloRolha.HR.feature.goout.service;
+
+import com.HelloRolha.HR.feature.goout.model.GooutType;
+import com.HelloRolha.HR.feature.goout.model.entity.*;
+import com.HelloRolha.HR.feature.goout.repo.GooutTypeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GooutTypeService {
+    private final GooutTypeRepository gooutTypeRepository;
+    public GooutType create(GooutTypeCreateReq gooutTypeCreateReq) {
+        GooutType gooutType = GooutType.builder()
+                .name(gooutTypeCreateReq.getName())
+                .detail(gooutTypeCreateReq.getDetail())
+                .maxHoliday(gooutTypeCreateReq.getMaxHoliday())
+                .includesWeekends(gooutTypeCreateReq.isIncludesWeekends())
+                .expiresAtYearEnd(gooutTypeCreateReq.isExpiresAtYearEnd())
+                .build();
+
+        return gooutTypeRepository.save(gooutType);
+    }
+
+    public List<GooutTypeList> list() {
+        List<GooutType> gooutTypes = gooutTypeRepository.findAll(); // 모든 게시글 조회
+        List<GooutTypeList> gooutTypeLists = new ArrayList<>();
+
+        for (GooutType gooutType : gooutTypes) {
+            GooutTypeList gooutTypeList = GooutTypeList.builder()
+                    .name(gooutType.getName())
+                    .build();
+            gooutTypeLists.add(gooutTypeList);
+        }
+        return gooutTypeLists;
+    }
+
+    public GooutTypeRead read(Integer id) {
+        GooutType gooutType = gooutTypeRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 GooutType이 존재하지 않습니다."));
+
+        return GooutTypeRead.builder()
+                .name(gooutType.getName())
+                .detail(gooutType.getDetail())
+                .maxHoliday(gooutType.getMaxHoliday())
+                .includesWeekends(gooutType.isIncludesWeekends())
+                .expiresAtYearEnd(gooutType.isExpiresAtYearEnd())
+                .build();
+    }
+
+    public GooutType update(GooutTypeUpdateReq gooutTypeUpdateReq) {
+        GooutType gooutType = gooutTypeRepository.findById(gooutTypeUpdateReq.getId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 GooutType이 존재하지 않습니다."));
+
+        gooutType.setName(gooutTypeUpdateReq.getName());
+        gooutType.setDetail(gooutTypeUpdateReq.getDetail());
+        gooutType.setMaxHoliday(gooutTypeUpdateReq.getMaxHoliday());
+        gooutType.setIncludesWeekends(gooutTypeUpdateReq.isIncludesWeekends());
+        gooutType.setExpiresAtYearEnd(gooutTypeUpdateReq.isExpiresAtYearEnd());
+
+        return gooutTypeRepository.save(gooutType);
+    }
+
+    public void delete(Integer id) {
+        GooutType gooutType = gooutTypeRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 GooutType이 존재하지 않습니다."));
+
+        gooutTypeRepository.delete(gooutType);
+    }
+
+}
